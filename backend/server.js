f682f3fba5904d419909eb06a51bcfa697f8c72d
Jyothiswaroopr <rjyothiswaroopreddy@gmail.com>
2025-04-01 11:49:58 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -66,5 +66,13 @@ app.post("/delete-task", (req, res) => {
         res.json({ message: "Task deleted" });
     });
 });
+const PORT = process.env.PORT || 3000; // Use Render's assigned port or default to 3000
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+
 
 
